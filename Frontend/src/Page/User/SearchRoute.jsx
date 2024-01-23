a51c3919b40263c98223f035a00ae8565daa5219
Suresh_Gobi <sureshgobi34@gmail.com>
2024-01-24 01:48:45 +0530

    added more route details

--- Frontend/src/Page/User/SearchRoute.jsx
+++ Frontend/src/Page/User/SearchRoute.jsx
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SearchRoutes = () => {
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
   const [searchResult, setSearchResult] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSearch = async () => {
     try {
-      const response = await fetch(`http://localhost:5000/api/users/search?from=${from}&to=${to}`);
+      const response = await fetch(
+        `http://localhost:5000/api/users/search?from=${from}&to=${to}`
+      );
       const data = await response.json();
 
       if (data.routes.length === 0) {
-        setErrorMessage('No routes found for the specified "from" and "to" values.');
+        setErrorMessage(
+          'No routes found for the specified "from" and "to" values.'
+        );
       } else {
         setSearchResult(data.routes);
-        setErrorMessage('');
+        setErrorMessage("");
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
-      setErrorMessage('Error fetching data. Please try again.');
+      console.error("Error fetching data:", error);
+      setErrorMessage("Error fetching data. Please try again.");
     }
   };
 
@@ -27,7 +31,11 @@ const SearchRoutes = () => {
     <div>
       <label>
         From:
-        <input type="text" value={from} onChange={(e) => setFrom(e.target.value)} />
+        <input
+          type="text"
+          value={from}
+          onChange={(e) => setFrom(e.target.value)}
+        />
       </label>
       <label>
         To:
@@ -35,14 +43,18 @@ const SearchRoutes = () => {
       </label>
       <button onClick={handleSearch}>Search Routes</button>
 
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
 
       {searchResult && (
         <div>
           <h3>Search Results:</h3>
           <ul>
             {searchResult.map((route) => (
-              <li key={route._id}>{route.name} - {route.from} to {route.to}</li>
+              <li key={route._id}>
+                {route.name} - {route.from} to {route.to} | Distance:{" "}
+                {route.distance} | Amount: {route.amount} | Duration:{" "}
+                {route.duration}
+              </li>
             ))}
           </ul>
         </div>
